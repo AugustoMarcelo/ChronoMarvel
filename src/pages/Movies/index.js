c60e33671f9ec965a355780711ea0e8cd78521ec
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-10 15:27:19 -0300

    Creating movies page

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,13 +1,77 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  FlatList,
+  Image,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+import Background from '../../components/Background';
+import { Shadow, Separator } from './styles';
+
+const styles = StyleSheet.create({
+  list: {
+    marginTop: 20,
+  },
+  cover: {
+    borderRadius: 4,
+    height: 218,
+    width: 150,
+    marginHorizontal: 10,
+  },
+});
+
 export default function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get('/movies', {
+        params: { order: 'chronology' },
+      });
+
+      setMovies(response.data.data);
+    }
+
+    loadMovies();
+  }, []);
+
   return (
-    <View>
-      <Text>Movies Page</Text>
-    </View>
+    <Background>
+      <Header />
+
+      {movies.length > 0 ? (
+        <FlatList
+          style={styles.list}
+          data={movies}
+          keyExtractor={movie => String(movie.id)}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <Shadow>
+              <TouchableOpacity>
+                <Image style={styles.cover} source={{ uri: item.cover_url }} />
+              </TouchableOpacity>
+            </Shadow>
+          )}
+        />
+      ) : (
+        <ActivityIndicator
+          style={{ marginTop: 20 }}
+          size={32}
+          color="#D63031"
+        />
+      )}
+
+      <Separator />
+    </Background>
   );
 }
 const tabBarIcon = ({ tintColor }) => (
---
+++ src/pages/Movies/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Shadow = styled.View`
+  elevation: 4;
+`;
+
+export const Separator = styled.View`
+  width: 50%;
+  height: 1px;
+  background: rgba(0, 0, 0, 0.1);
+  margin: 10px 0;
+`;
