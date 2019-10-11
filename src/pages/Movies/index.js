de20278573445d9e2270437efb4a774902f0e459
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-11 08:48:45 -0300

    Creating movies page interface

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
-  View,
   FlatList,
+  View,
+  Text,
   Image,
   ActivityIndicator,
   StyleSheet,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -14,7 +16,7 @@ import api from '../../services/api';
 
 import Header from '../../components/Header';
 import Background from '../../components/Background';
-import { Shadow, Separator } from './styles';
+import { Shadow, Separator, Card, CardTop, MoreInfo } from './styles';
 
 const styles = StyleSheet.create({
   list: {
@@ -24,53 +26,158 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     height: 218,
     width: 150,
-    marginHorizontal: 10,
+    // marginHorizontal: 10,
+  },
+  label: {
+    fontSize: 13,
+    color: '#ccc',
+  },
+  info: {
+    fontSize: 15,
+    color: '#333',
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 24,
+    color: '#333',
+    marginVertical: 5,
+    fontWeight: 'bold',
+  },
+  description: {
+    color: '#444',
+    textAlign: 'justify',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#00B894',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 48,
+    borderRadius: 2,
+    elevation: 1,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+    letterSpacing: 2,
   },
 });
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
+  const [selected, setSelected] = useState({});
+  const [error, setError] = useState('');
+
+  const flatList = useRef(null);
 
   useEffect(() => {
     async function loadMovies() {
-      const response = await api.get('/movies', {
-        params: { order: 'chronology' },
-      });
+      try {
+        const response = await api.get('/movies', {
+          params: { order: 'chronology' },
+        });
 
-      setMovies(response.data.data);
+        setMovies(response.data.data);
+      } catch (err) {
+        if (err.status === 500) {
+          setError('Unable to connect to server');
+        }
+      }
     }
 
     loadMovies();
+    // flatList.current.scrollToIndex({ animated: true, index: 5 });
   }, []);
+
+  function handleSelectMovie(id) {
+    const [movie] = movies.filter(item => item.id === id);
+    movie.release_date = movie.release_date.split('-', 1)[0];
+    setSelected(movie);
+  }
 
   return (
     <Background>
       <Header />
+      <ScrollView>
+        {movies.length > 0 ? (
+          <FlatList
+            ref={flatList}
+            style={styles.list}
+            data={movies}
+            keyExtractor={movie => String(movie.id)}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Shadow>
+                <TouchableOpacity onPress={() => handleSelectMovie(item.id)}>
+                  <Image
+                    style={styles.cover}
+                    source={{ uri: item.cover_url }}
+                  />
+                </TouchableOpacity>
+              </Shadow>
+            )}
+          />
+        ) : (
+          <ActivityIndicator
+            style={{ marginTop: 20 }}
+            size={32}
+            color="#D63031"
+          />
+        )}
 
-      {movies.length > 0 ? (
-        <FlatList
-          style={styles.list}
-          data={movies}
-          keyExtractor={movie => String(movie.id)}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <Shadow>
-              <TouchableOpacity>
-                <Image style={styles.cover} source={{ uri: item.cover_url }} />
-              </TouchableOpacity>
-            </Shadow>
-          )}
-        />
-      ) : (
-        <ActivityIndicator
-          style={{ marginTop: 20 }}
-          size={32}
-          color="#D63031"
-        />
-      )}
+        <Separator />
 
-      <Separator />
+        {selected.id > 0 ? (
+          <Card>
+            <CardTop>
+              <View
+                style={{
+                  borderRadius: 4,
+                  backgroundColor: '#fff',
+                  elevation: 2,
+                }}
+              >
+                <Image
+                  style={{ height: 318, width: 219, borderRadius: 2 }}
+                  source={{
+                    uri: selected.cover_url && selected.cover_url,
+                  }}
+                />
+              </View>
+              <MoreInfo>
+                <View>
+                  <Text style={styles.label}>Duration</Text>
+                  <Text style={styles.info}>{`${selected.duration} min`}</Text>
+                </View>
+                <View>
+                  <Text style={styles.label}>Year</Text>
+                  <Text style={styles.info}>{selected.release_date}</Text>
+                </View>
+                <View>
+                  <Text style={styles.label}>Phase</Text>
+                  <Text style={styles.info}>{selected.phase}</Text>
+                </View>
+                <View>
+                  <Text style={styles.label}>Saga</Text>
+                  <Text style={styles.info}>{selected.saga}</Text>
+                </View>
+              </MoreInfo>
+            </CardTop>
+            <Text style={styles.title}>{selected.title}</Text>
+            <Text style={styles.description}>{selected.overview}</Text>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Mark as watched!</Text>
+            </TouchableOpacity>
+          </Card>
+        ) : (
+          <Card style={{ alignItems: 'center' }}>
+            <Text style={{ color: '#D63031', fontSize: 18, letterSpacing: 2 }}>
+              No movies selected
+            </Text>
+          </Card>
+        )}
+      </ScrollView>
     </Background>
   );
 }
--- src/pages/Movies/styles.js
+++ src/pages/Movies/styles.js
@@ -1,12 +1,32 @@
 import styled from 'styled-components/native';
 
 export const Shadow = styled.View`
+  margin: 0 10px 5px;
+  border-radius: 4px;
+  background: #fff;
   elevation: 4;
 `;
 
 export const Separator = styled.View`
   width: 50%;
-  height: 1px;
+  align-self: center;
+  height: 2px;
   background: rgba(0, 0, 0, 0.1);
-  margin: 10px 0;
+  margin: 10px;
+`;
+
+export const Card = styled.View`
+  margin: 0 10px 10px;
+  padding: 10px;
+  background: #fff;
+  elevation: 2;
+  border-radius: 2px;
+`;
+
+export const CardTop = styled.View`
+  flex-direction: row;
+`;
+
+export const MoreInfo = styled.View`
+  margin-left: 10px;
 `;
