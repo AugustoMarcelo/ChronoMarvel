052a56b899b3667f247c4d6f85b5c658efb45314
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-12 09:58:00 -0300

    Updated watched interface with real data

--- src/pages/Watched/index.js
+++ src/pages/Watched/index.js
@@ -1,16 +1,77 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, FlatList } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { parseISO, formatDistance } from 'date-fns';
+
+import getRealm from '../../services/realm';
 
 import Header from '../../components/Header';
 import Background from '../../components/Background';
+import FAB from '../../components/FAB';
 
 export default function Watched() {
+  const [watched, setWatched] = useState([]);
+
+  useEffect(() => {
+    async function loadWatchedMovies() {
+      const realm = await getRealm();
+
+      const data = realm.objects('Watched').sorted('watched', true);
+
+      setWatched(
+        data.map(item => ({
+          ...item,
+          watchedTime: formatDistance(
+            parseISO(item.watched),
+            parseISO(new Date().toISOString()),
+            {
+              addSuffix: true,
+            }
+          ),
+        }))
+      );
+    }
+
+    loadWatchedMovies();
+  }, [watched]);
+
+  function handleRemoveAll() {
+    setWatched([]);
+  }
+
   return (
     <Background>
       <Header />
-      <Text>Watched Page</Text>
+      <FlatList
+        data={watched}
+        style={{ backgroundColor: '#fff' }}
+        keyExtractor={item => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: 'row',
+              padding: 10,
+              borderBottomWidth: 1,
+              borderBottomColor: '#ccc',
+            }}
+          >
+            <Image
+              style={{ height: 83, width: 58, borderRadius: 2 }}
+              source={{ uri: item.cover_url }}
+            />
+            <View style={{ marginLeft: 10, justifyContent: 'space-between' }}>
+              <Text style={{ fontSize: 17 }}>{item.title}</Text>
+              <Text style={{ fontSize: 13, color: '#ccc' }}>
+                {item.watchedTime}
+              </Text>
+              <Icon name="check" size={28} color="#00b894" />
+            </View>
+          </View>
+        )}
+      />
+      {watched.length > 0 && <FAB onRemoveAll={handleRemoveAll} />}
     </Background>
   );
 }
