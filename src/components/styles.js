3947657067c1780acbde9554d487e501d78a665c
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-14 12:00:40 -0300

    Created styles to put reusable components

---
+++ src/components/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Card = styled.View`
+  margin: 0 10px 10px;
+  padding: 10px;
+  background: #fff;
+  elevation: 2;
+  border-radius: 2px;
+`;
